05fa96d112ab197ef9d76c92926c4024840aee8b
RohithKumarV2004 <rohithkumarv2004@gmail.com>
2023-07-02 18:28:11 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,78 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const homeStartingContent = "Discover, share, and learn on Self QnA - the platform where you can ask questions and provide your own answers. Join a vibrant community of knowledge seekers and contribute to a growing repository of wisdom. Get the answers you need, share your expertise, and empower others. Start your knowledge journey with Self QnA today!";
+const aboutContent = "Welcome to Self QnA, a platform built on the belief that knowledge is best shared through personal experiences and insights. Our mission is to empower individuals to ask questions, provide their own answers, and create a community-driven resource for knowledge sharing.";
+const contactContent = "We value your feedback, suggestions, and inquiries. If you have any questions or need assistance, please don't hesitate to reach out to us. We're here to help!";
+
+const app = express();
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public")); 
+
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+app.get("/", function(req, res){
+
+  Post.find({}, function(err, posts){
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+      });
+  });
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
+
+
+  post.save(function(err){
+    if (!err){
+        res.redirect("/");
+    }
+  });
+});
+
+app.get("/posts/:postId", function(req, res){
+
+const requestedPostId = req.params.postId;
+
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+  });
+
+});
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
